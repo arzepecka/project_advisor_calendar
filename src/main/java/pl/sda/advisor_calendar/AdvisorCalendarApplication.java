b5cdb056d2cd2d06f021a2bcd3e477d0868192db
rzepecka <rzepecka00@gmail.com>
2019-09-07 10:16:57 +0200

    basic

---
+++ src/main/java/pl/sda/advisor_calendar/AdvisorCalendarApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.advisor_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvisorCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/advisor_calendar/AdvisorCalendarApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.advisor_calendar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvisorCalendarApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
