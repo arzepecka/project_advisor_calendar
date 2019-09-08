1d8157c218e018daa69716cef7ba66e288ccb7fe
rzepecka <rzepecka00@gmail.com>
2019-09-08 13:48:16 +0200

    rozpoczecie pracy nad indexem i kalendarzem

--- src/main/java/pl/sda/advisor_calendar/web/MainViewController.java
+++ src/main/java/pl/sda/advisor_calendar/web/MainViewController.java
@@ -3,6 +3,7 @@ package pl.sda.advisor_calendar.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import pl.sda.advisor_calendar.model.user.UserDTO;
 
 @Controller
 class MainViewController {
@@ -10,6 +11,7 @@ class MainViewController {
     @RequestMapping
     ModelAndView mainView() {
         ModelAndView mav = new ModelAndView("index.html");
+        mav.addObject(new UserDTO());
         return mav;
     }
 }
